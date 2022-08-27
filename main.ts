c8aad87f280b48a3d625a5e15c2d8f0edba7954b
unknown <odh1995>
2022-08-27 20:38:33 +0800

    added onsay function

--- main.ts
+++ main.ts
@@ -10,6 +10,13 @@ let zepLogo = ScriptApp.loadSpritesheet("zep_logo.png");
 
 ScriptMap.putObject(0, 0, zepLogo, { overlap: true });
 
+let _answer = "ZEP"
+ScriptApp.onSay.Add(function(player, text){
+  if (_answer == text){
+    ScriptApp.showCenterLabel(player.name + ' Correct!\nThe answer is ' + _answer);
+  }
+})
+
 ScriptApp.onDestroy.Add(function () {
   ScriptMap.clearAllObjects();
 });
